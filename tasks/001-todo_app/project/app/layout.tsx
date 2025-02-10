bcdce5f87c4b9218d537fb63b7de477e4406bc2e
Mike Wheaton <mike@wheaton.dev>
2025-02-10 09:52:40 +0800

    Clean up to match other projects

--- tasks/001-todo_app/project/app/layout.tsx
+++ tasks/001-todo_app/project/app/layout.tsx
@@ -1,18 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "react-hot-toast";
 import { BackendContext } from "@/lib/BackendContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "TODO App",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased bg-slate-950">
         <BackendContext>{children}</BackendContext>
         <Toaster
           position="top-center"
--- tasks/002-files_app/project/app/layout.tsx
+++ tasks/002-files_app/project/app/layout.tsx
@@ -1,18 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import { ProjectSidebar } from "./ProjectSidebar";
 import { BackendContext } from "@/lib/BackendContext";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import { ProjectSidebar } from "./ProjectSidebar";
 
 export const metadata: Metadata = {
   title: "Files Workspace",
@@ -25,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en">
+      <body className="antialiased bg-slate-950">
         <BackendContext>
           <div className="flex h-screen">
             <ProjectSidebar />
