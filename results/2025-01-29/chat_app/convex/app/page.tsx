0c45ac5378385fc9c9259d4a2b53e01f30e017b1
Sujay Jayakar <sujayakar@convex.dev>
2025-02-11 10:00:19 -0500

    Move results to branch

--- results/2025-01-29/chat_app/convex/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { LoginForm } from "@/components/LoginForm";
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  // Redirect to the general channel
-  redirect('/channels/general');
-}
